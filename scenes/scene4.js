2180a0758c800f857eb90bce858fa29a6031d2e7
Julian León <juliancho3452@gmail.com>
2021-09-09 17:01:25 -0500

    creating levels 2 and 3.
    They are in comments, besides that I put indications of certain necessary values per level which are:
    - goal limit
    - camera limit
    - number of platforms
    - limit where you win
    
    I created a variable that indicates the number of platforms to create and their respective decorations (rocks). The name of the varable is "numPlatforms"

--- scenes/scene4.js
+++ scenes/scene4.js
@@ -26,15 +26,60 @@ class Scene4 extends Phaser.Scene {
 
 		//this.physics.world.bounds.width = 
 		
-		
-		//x minimo : 400
-		//y  maximo 900 , minimo 650
-		//minimo 10 plataformas maximo 16
+/* LIMITS
+	(eje x) minimo : 400
+	(eje y) maximo 900 (-y), minimo 650(+y)
+	minimo 10 plataformas maximo 16
+*/
+
+/*=============================
+FIRST LEVEL (Tutorial)
+
+- numPlatforms : 9
+- goal limit : 5100
+- win : 4800
+- camera : 5760
+*/
 
 		level_platformsX = [100, 600, 1200, 1800, 2500, 3000, 3700, 4300, 4800];
 		level_platformsY = [900, 850, 800,  800,  800,  700,  800,  700,  850];
 		level_platformsColor = [1, 2, 3,  4,  5,  6,  7,  4,  2];
-		
+        let numPlatforms = 9; // Number of platforms per level
+
+
+/*=============================
+SECOND LEVEL (Aprobado?)
+
+- numPlatforms : 12
+- goal limit : 7600
+- win : 7600
+- camera : 8300
+
+		level_platformsX = [110, 780, 1400, 2000, 2650, 3200, 3800, 4250, 4800, 5400, 6000, 6800 ];
+		level_platformsY = [900, 780, 630,  900,  710,  850,  620,  870, 810, 730, 640, 880];
+		level_platformsColor = [5, 1, 3,  1,  7,  6,  2,  1,  5, 3, 7, 1];
+        let numPlatforms = 12; // Number of platforms per level
+*/
+
+
+/*=============================
+THIRD LEVEL (Aprobado?)
+
+- numPlatforms : 11
+- goal limit : 7800
+- win : 7800
+- camera : 8300
+
+		level_platformsX = [100, 960, 1600, 2350, 2900, 3600, 4200, 4850, 5500, 6400, 7000];
+		level_platformsY = [630, 780, 830, 700,  850,  620,  870, 750, 640, 800, 720];
+		level_platformsColor = [5, 1, 3, 7,  6,  2,  1, 3, 5, 1, 6];
+        let numPlatforms = 11; // Number of platforms per level
+*/
+
+
+
+
+
 		/*player*/
 		//game.player  = this.physics.add.image(200, 300, 'player').setOrigin(0, 0);
 		game.player  = this.physics.add.image(250, 100, 'player1');
@@ -55,15 +100,16 @@ class Scene4 extends Phaser.Scene {
     	
     	currentPlatform = game.lastPlatform ? game.lastPlatform: 0;
     	canLaunch = true;
-    	
-    	for(var i = 0; i< 9; i++){
+
+
+    	for(var i = 0; i< numPlatforms; i++){
     		addPlatform(i);	
     	}
     	_this.add.image(0, 990, 'backgroundBottom1A', 0).setOrigin(0,0);
     	_this.add.image(2880, 990, 'backgroundBottom1B', 0).setOrigin(0,0);		
 		
 
-    	for(var i = 0; i< 9; i++){
+    	for(var i = 0; i< numPlatforms; i++){
     		_this.physics.add.image(platforms[i].x - 60, game.config.height - 120, 'platformBottom').setOrigin(0,0);	
     	}
 
